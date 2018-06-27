42ffac76fa910125f55119f35556e3c64e2b517c
Max (Raran) <raranmariano@hotmail.com>
2018-06-27 11:31:47 -0300

    main comms

--- main.js
+++ main.js
@@ -2,20 +2,24 @@ const Discord = require('discord.js');
 const configs = require('./config.json')
 const client = new Discord.Client();
 
+//starting the bot set its activity and logs in the console some info
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`)
     
     client.user.setActivity(`OwO what's this?`);
 });
 
+//joining a server will log in the console where and how many people in there
 client.on("guildCreate", guild => {
     console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
 });
 
+//been removed from a server
 client.on("guildDelete", guild => {
     console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
 });
 
+//any message read
 client.on('message', msg => {
 
     if (msg.author.bot) return;
@@ -37,4 +41,5 @@ client.on('message', msg => {
     }
 });
 
+//bot start
 client.login(configs.token);
